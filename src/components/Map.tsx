df7cc7eb034ee6f03422b0e59ad5d0c2d6500f4f
Gkasflows <gkasflows@gmail.com>
2026-04-07 13:16:32 +0100

    UI Polish: Gently reduced map brightness

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -578,7 +578,7 @@ export default function Map() {
           data: undefined,
           image: props.data,
           bounds: [boundingBox[0][0], boundingBox[0][1], boundingBox[1][0], boundingBox[1][1]],
-          tintColor: [60, 68, 80, 255] // Extremely Dimmed Deep Slate aesthetic for maximum UI pop
+          tintColor: [110, 120, 135, 255] // Slightly dimmed sleek slate aesthetic
         });
       }
     }),
